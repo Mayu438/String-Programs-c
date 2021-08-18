35f4c86e49f9cf5700ce0dcff57136af95f14ed0
Mayu438 <88482920+Mayu438@users.noreply.github.com>
2021-08-18 14:26:34 +0530

    Add files via upload

---
+++ 26string.c
@@ -0,0 +1,27 @@
+/*Write a C program which accept two strings from user and compare only first N characters of two
+strings. If both strings are equal till first N characters then return 0 otherwise return difference
+between first mismatch character.
+Eg:
+Input String1:
+Ramayan
+Input String2:
+Ramanacharya
+Input of N:
+4
+Output:
+Both strings are equal.+
+
+AUTHOR:MAYURI NARAYAN DIGHE.
+BATCH:122_PPA7..*/
+
+#include<stdio.h>
+void main()
+{
+	char str[100],str1[100];
+	printf("ENTER A 1ST STRING:\n");
+	fgets(str,sizeof(str),stdin);
+	printf("ENTER A 2ND STRING:\n");
+	fgets(str1,sizeof(str1),stdin);
+	
+	while(str[i]!='\n')
+}
