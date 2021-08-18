9a3d3b287e4d291785ff3c0ee4534dfeb38871ed
Mayu438 <88482920+Mayu438@users.noreply.github.com>
2021-08-18 14:20:39 +0530

    Add files via upload

---
+++ 10string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	char str[100];
+	printf("ENTER A STRING:\n");
+	fgets(str,sizeof(str),stdin);
+	printf("GIVEN STRING IS:");
+	printf("%s",str);
+	
+	while(str[i]!='\n')
+	{
+	
+	}
+	
+
+}
